b5deb088997742c13d17d8cc0d78b7c30985bfcb
Andy Waplinger <andywaplinger@users.noreply.github.com>
2024-02-21 23:51:26 -0500

    Update ESP32_HomeKit_LED.ino

--- ESP32_HomeKit_LED.ino
+++ ESP32_HomeKit_LED.ino
@@ -17,14 +17,15 @@
  * Setup notes:
  *  You must choose a Partition Scheme that supports OTA but minimizes SPIFFS
  *   I'm using "Minimal SPIFFS (1.9MB APP with OTA/190KB SPIFFS)"
- *   If not possible or WebSerial isn't needed, WebSerial can be removed
+ *   If WebSerial is too large or isn't needed, it can be removed
  *  You must disable USB serial input to power the ESP32 with 5V input and not a USB cable
- *   HomeSpan will otherwise get stuck in a loop looking for serial input that doesn't exist and not load
+ *   HomeSpan will otherwise get stuck in a loop looking for serial input that doesn't exist and then never boot
  *  Data and Control pins can be changed to other GPIO pins
  *  HomeKit code can be changed during the Wi-Fi configuration process
  *   Default is 466-37-726
  *  OTA password can be changed through USB serial input in the Arduino IDE
  *   Default is "homespan-ota"; send the letter "O" through the serial monitor to change OTA password
+ *   Don't forget to disable serial input after you're done
  */
 
 /////////////////////////////////////////////// [ Libraries ] ///////////////////////////////////////////////////
@@ -40,15 +41,15 @@
 
 ///////////////////////////////////////////// [ Key Variables ] /////////////////////////////////////////////////
 
-#define NUM_PIXELS             95                          // Number of pixels in LED strip
-char* name =                   "L Office Credenza";        // Customize controller's name, will be shown in the serial output
-bool disableUSBserial =        true;                       // Disables USB serial input, allowing HomeSpan to boot without USB cable
-bool enableWebSerial =         true;                       // Enables all items for WebSerial
-String hostname =              String(name);               // Used to change the device's hostname to reflect its name; e.g., name "Media Center" will become hostname "media-center"
+#define NUM_PIXELS             100                          // Number of pixels in LED strip
+char* name =                   "Pixel LED";                 // Customize controller's name, will be shown in the serial output
+bool disableUSBserial =        false;                       // Disables USB serial input, allowing HomeSpan to boot without a USB cable
+bool enableWebSerial =         false;                       // Enables WebSerial server
+String hostname =              String(name);                // Device's hostname generated from its name; e.g., name "Media Center" will become hostname "media-center"
 
-#define NEOPIXEL_RGB_PIN       15                           // Data pin for LED strip
-#define STA_PIN                2                            // Onboard status LED on ESP32
-#define CTL_PIN                4                            // Control pin for physical button
+#define NEOPIXEL_PIN       15                               // Data pin for LED strip
+#define STA_PIN                2                            // Onboard status LED on ESP32, optional
+#define CTL_PIN                4                            // Control pin for physical button, optional
 
 /////////////////////////////////////////////// [ WebSerial ] ///////////////////////////////////////////////////
 
@@ -71,7 +72,7 @@ void recvMsg(uint8_t *data, size_t len){
 
 ////////////////////////////////////////////// [ NeoPixel RGB ] /////////////////////////////////////////////////
 
-struct NeoPixel_RGB : Service::LightBulb {      // Addressable single-wire RGB LED strand (e.g. NeoPixel)
+struct NeoPixel_RGB : Service::LightBulb {                      // Addressable single-wire RGB LED strand (e.g. NeoPixel)
  
   Characteristic::On power{0,true};
   Characteristic::Hue H{0,true};
@@ -82,23 +83,23 @@ struct NeoPixel_RGB : Service::LightBulb {      // Addressable single-wire RGB L
   
   NeoPixel_RGB(uint8_t pin, int nPixels) : Service::LightBulb(){
 
-    V.setRange(1,100,1);                      // Range of Brightness has a min of 1%, a max of 100%, in steps of 1%
-    pixel=new Pixel(pin);                     // Creates Pixel LED on specified pin
-    this->nPixels=nPixels;                    // Save number of Pixels in this LED strip
-    update();                                 // Manually call update() to set pixel with restored initial values
+    V.setRange(1,100,1);                                        // Range of Brightness has a min of 1%, a max of 100%, in steps of 1%
+    pixel=new Pixel(pin);                                       // Creates Pixel LED on specified pin
+    this->nPixels=nPixels;                                      // Save number of Pixels in this LED strip
+    update();                                                   // Manually call update() to set pixel with restored initial values
   }
 
   boolean update() override {
 
     int p=power.getNewVal();
     
-    float h=H.getNewVal<float>();       // range = [0,360]
-    float s=S.getNewVal<float>();       // range = [0,100]
-    float v=V.getNewVal<float>();       // range = [0,100]
+    float h=H.getNewVal<float>();                               // range = [0,360]
+    float s=S.getNewVal<float>();                               // range = [0,100]
+    float v=V.getNewVal<float>();                               // range = [0,100]
 
     Pixel::Color color;
 
-    pixel->set(color.HSV(h*p, s*p, v*p),nPixels);       // sets all nPixels to the same HSV color
+    pixel->set(color.HSV(h*p, s*p, v*p),nPixels);               // sets all nPixels to the same HSV color
           
     return(true);  
   }
@@ -106,7 +107,7 @@ struct NeoPixel_RGB : Service::LightBulb {      // Addressable single-wire RGB L
 
 ////////////////////////////////////////////// [ NeoPixel RGBW ] ////////////////////////////////////////////////
 
-struct NeoPixel_RGBW : Service::LightBulb {      // Addressable single-wire RGBW LED Strand (e.g. NeoPixel)
+struct NeoPixel_RGBW : Service::LightBulb {                     // Addressable single-wire RGBW LED Strand (e.g. NeoPixel)
  
   Characteristic::On power{0,true};
   Characteristic::Brightness V{100,true};
@@ -116,24 +117,24 @@ struct NeoPixel_RGBW : Service::LightBulb {      // Addressable single-wire RGBW
   
   NeoPixel_RGBW(uint8_t pin, int nPixels) : Service::LightBulb(){
 
-    V.setRange(5,100,1);                      // sets the range of the Brightness to be from a min of 5%, to a max of 100%, in steps of 1%
-    pixel=new Pixel(pin,true);                // creates Pixel RGBW LED (second parameter set to true for RGBW) on specified pin
-    this->nPixels=nPixels;                    // save number of Pixels in this LED Strand
-    update();                                 // manually call update() to set pixel with restored initial values
+    V.setRange(5,100,1);                                        // sets the range of the Brightness to be from a min of 5%, to a max of 100%, in steps of 1%
+    pixel=new Pixel(pin,true);                                  // creates Pixel RGBW LED (second parameter set to true for RGBW) on specified pin
+    this->nPixels=nPixels;                                      // save number of Pixels in this LED Strand
+    update();                                                   // manually call update() to set pixel with restored initial values
   }
 
   boolean update() override {
 
     int p=power.getNewVal();
     
-    float v=V.getNewVal<float>();       // range = [0,100]
-    float t=T.getNewVal<float>();       // range = [140,500] (140=coldest, 500=warmest)
+    float v=V.getNewVal<float>();                               // range = [0,100]
+    float t=T.getNewVal<float>();                               // range = [140,500] (140=coldest, 500=warmest)
 
-    float hue=240-(t-140)/3;            // add in a splash of color between blue and green to simulated change of color temperature
+    float hue=240-(t-140)/3;                                    // add in a splash of color between blue and green to simulated change of color temperature
 
-    // Pixel::Color color;              // if static HSV method is used (below), there is no need to first create a Color object
+    // Pixel::Color color;                                      // if static HSV method is used (below), there is no need to first create a Color object
 
-    pixel->set(pixel->HSV(hue, 100, v*p, v*p),nPixels);       // sets all nPixels to the same HSV color (note use of static method pixel->HSV, instead of defining and setting Pixel::Color)
+    pixel->set(pixel->HSV(hue, 100, v*p, v*p),nPixels);         // sets all nPixels to the same HSV color (note use of static method pixel->HSV, instead of defining and setting Pixel::Color)
           
     return(true);  
   }
@@ -159,18 +160,18 @@ void setup() {
   homeSpan.begin(Category::Lighting,name,hostname.c_str());
 
   if (enableWebSerial == true) {
-    WiFi.mode(WIFI_STA);                                                  // Required for WebSerial to work
-    WebSerial.begin(&server);                                             // Accessible at http://<IP Address>:8080/webserial
-    WebSerial.msgCallback(recvMsg);                                         // Attach message callback
+    WiFi.mode(WIFI_STA);                                        // Required for WebSerial to work
+    WebSerial.begin(&server);                                   // Accessible at http://<IP Address>:8080/webserial
+    WebSerial.msgCallback(recvMsg);                             // Attach message callback
     server.begin();
   };
 
   SPAN_ACCESSORY("Pixel RGB");
-    new NeoPixel_RGB(NEOPIXEL_RGB_PIN,NUM_PIXELS);                      // Create NeoPixel RGB Strand with full color control
+    new NeoPixel_RGB(NEOPIXEL_PIN,NUM_PIXELS);                  // Create NeoPixel RGB Strand with full color control
   
   /*
   SPAN_ACCESSORY("Neo RGBW");
-    new NeoPixel_RGBW(NEOPIXEL_RGBW_PIN,NUM_PIXELS);                    // create NeoPixel RGBW Strand with simulated color temperature control 
+    new NeoPixel_RGBW(NEOPIXEL_RGBW_PIN,NUM_PIXELS);            // create NeoPixel RGBW Strand with simulated color temperature control 
   */
 }
 
